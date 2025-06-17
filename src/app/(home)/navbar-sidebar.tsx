ad71cee881230985c53488f89194a0e848eff211
johnson-dsouza-dev <jdsouza2002dev@gmail.com>
2025-06-17 11:45:48 +0530

    this is 03: home layout commit

---
+++ src/app/(home)/navbar-sidebar.tsx
@@ -0,0 +1,62 @@
+interface NavbarItem {
+  href: string;
+  children: React.ReactNode;
+}
+
+interface Props {
+  items: NavbarItem[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import Link from "next/link";
+import React from "react";
+
+export const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <div className="flex items-center">
+            <SheetTitle>Menu</SheetTitle>
+          </div>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.href}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              href={"/sign-in"}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              LOGIN
+            </Link>
+            <Link
+              href={"/sign-up"}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              STAT SELLING
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
